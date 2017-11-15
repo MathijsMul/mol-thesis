5682272eb09bcac76b847cd228f1aa85b93c7b7f
MathijsMul <mathijsmul@gmail.com>
2017-11-15 14:57:31 +0100

    50 epochs

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 main.py './data/binary/2dets_4negs/binary_2dets_4negs_train.txt' './data/binary/2dets_4negs/binary_2dets_4negs_test.txt' sumNN 1 > bin_2dets_4negs_sumnn.txt &
+python3 main.py './data/binary/2dets_4negs/binary_2dets_4negs_train.txt' './data/binary/2dets_4negs/binary_2dets_4negs_test.txt' sumNN 50 > bin_2dets_4negs_sumnn.txt &
 
-python3 main.py './data/binary/2dets_4negs/binary_2dets_4negs_train.txt' './data/binary/2dets_4negs/binary_2dets_4negs_test.txt' tRNN 1 > bin_2dets_4negs_trnn.txt &
+python3 main.py './data/binary/2dets_4negs/binary_2dets_4negs_train.txt' './data/binary/2dets_4negs/binary_2dets_4negs_test.txt' tRNN 50 > bin_2dets_4negs_trnn.txt &
 
-python3 main.py './data/binary/2dets_4negs/binary_2dets_4negs_train.txt' './data/binary/2dets_4negs/binary_2dets_4negs_test.txt' tRNTN 1 > bin_2dets_4negs_trntn.txt &
+python3 main.py './data/binary/2dets_4negs/binary_2dets_4negs_train.txt' './data/binary/2dets_4negs/binary_2dets_4negs_test.txt' tRNTN 50 > bin_2dets_4negs_trntn.txt &

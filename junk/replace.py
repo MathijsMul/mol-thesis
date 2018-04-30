709794247c330c6794dc1f6e17eea20d714e9f2d
Mathijs <mathijsmul@gmail.com>
2018-04-30 17:40:58 +0200

    removed old files

--- junk/replace.py
+++
@@ -1,4 +0,0 @@
-sm_fan_in = self.sm.weight.size(1)
-sm_fan_out = self.sm.weight.size(0)
-self.sm.weight = xavier_uniform_adapted(self.sm.weight, sm_fan_in, sm_fan_out)
-self.sm.bias = xavier_uniform_adapted(self.sm.bias, sm_fan_in, sm_fan_out)
